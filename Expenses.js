d7dfab1d5354941143739cbbfe146f62f5d7726a
PinnojuSaikiran <98552621+PinnojuSaikiran@users.noreply.github.com>
2022-05-20 23:56:19 +0530

    Add files via upload
    1st react project

---
+++ Expenses.js
@@ -0,0 +1,53 @@
+import React,{useState} from 'react';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
+import ExpensesList from './ExpensesList';
+import Card from '../UI/Card';
+import './Expenses.css';
+
+const Expenses = (props) => {
+
+  const [filteredYear,setFilteredYear] = useState("2020");
+
+  function filterChangeHandler(selectedYear){
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = props.items.filter(expense =>{
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  
+  return (
+  <div>
+    <Card className="expenses">
+    <ExpensesFilter 
+    selected={filteredYear} 
+    onChangeFilter={filterChangeHandler}
+    />
+    <ExpensesChart expenses={filteredExpenses}/>
+    <ExpensesList items= {filteredExpenses}/>    
+    </Card>
+  </div>
+  );
+}
+
+export default Expenses;
+
+{/* {filteredExpenses.length === 0 ? (<p> No Expenses Found. </p>) : (filteredExpenses.map(expense => 
+    <ExpenseItem 
+    key={expense.id}
+    title={expense.title} 
+    amount={expense.amount} 
+    date={expense.date} />))}
+
+    0r we can use 
+    
+    {filteredExpenses.length === 0 && <p> No Expenses Found. </p>}
+    {filteredExpenses.length > 0 && 
+      filteredExpenses.map((expense => (
+      <ExpenseItem 
+      key={expense.id}
+      title={expense.title} 
+      amount={expense.amount} 
+      date={expense.date} />
+      )))} */}
